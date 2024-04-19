d5a06b36026d414fe0a714e1718e792197fb3bfc
JosephDonoso <joseph.donoso.c@mail.pucv.cl>
2024-04-19 19:43:21 -0400

    Se crean los selectores de regiones y comunas

--- Scripts/registrate.js
+++ Scripts/registrate.js
@@ -17,3 +17,52 @@
         }, false)
     })
 })()
+
+const selector_regiones = document.querySelector("#lista-regiones")
+
+fetch('../Database/comunas-regiones.json')
+    .then(respuesta => respuesta.json())
+    .then(regiones => {
+        let cont = 1;
+        regiones.forEach( region => {
+            let option = document.createElement('option');
+            option.value = region.region;
+            option.innerHTML += `${region.region}`;
+            selector_regiones.appendChild(option);
+            cont += 1;
+        })
+        
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+const selector_comunas = document.querySelector("#lista-comunas");
+selector_regiones.addEventListener('change', function(event) {
+    let opcion_default = document.querySelector("#comuna-default");
+    selector_comunas.innerHTML = '';
+    selector_comunas.appendChild(opcion_default);
+    selector_comunas.value = "";
+
+    fetch('../Database/comunas-regiones.json')
+        .then(respuesta => respuesta.json())
+        .then(regiones => {
+            let region_escogida = selector_regiones.value;
+            let cont = 1;
+            regiones.forEach( region => {
+                if(region.region === region_escogida){
+                    region.comunas.forEach( comuna => {
+                        let option = document.createElement('option');
+                        option.value = comuna;
+                        option.innerHTML += `${comuna}`;
+                        selector_comunas.appendChild(option);
+                        cont += 1;
+                    })
+                }
+            })
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
